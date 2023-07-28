c3997b8e5268c1d2039bd07ed026ec0665a4b21c
박성현 <pjhomg5530@naver.com>
2023-07-28 17:49:09 +0900

    :lipstick: :: Icon 생성하기

---
+++ GYMI-components/src/main/java/com/mpersand/gymi_components/theme/GYMIIcon.kt
@@ -0,0 +1,229 @@
+package com.mpersand.gymi_components.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.material.Icon
+import com.mpersand.gymi_components.R
+
+@Composable
+fun BackArrow(
+    modifier: Modifier = Modifier,
+    tint: Color = Color.Unspecified
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_back_arrow),
+        contentDescription = "backArrow",
+        modifier = modifier,
+        tint = tint
+    )
+}
+
+@Composable
+fun Badminton(
+    modifier: Modifier = Modifier,
+    tint: Color = Color.Unspecified
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_badminton),
+        contentDescription = "badminton",
+        modifier = modifier,
+        tint = tint
+    )
+}
+
+@Composable
+fun BasketBall(
+    modifier: Modifier = Modifier,
+    tint: Color = Color.Unspecified
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_basketball),
+        contentDescription = "basketball",
+        modifier = modifier,
+        tint = tint
+    )
+}
+
+@Composable
+fun Bell(
+    modifier: Modifier = Modifier,
+    tint: Color = Color.Unspecified
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_bell),
+        contentDescription = "bell",
+        modifier = modifier,
+        tint = tint
+    )
+}
+
+@Composable
+fun CheckBox(
+    modifier: Modifier = Modifier,
+    tint: Color = Color.Unspecified
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_check_box),
+        contentDescription = "checkBox",
+        modifier = modifier,
+        tint = tint
+    )
+}
+
+@Composable
+fun Delete(
+    modifier: Modifier = Modifier,
+    tint: Color = Color.Unspecified
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_delete),
+        contentDescription = "delete",
+        modifier = modifier,
+        tint = tint
+    )
+}
+
+@Composable
+fun Equipment(
+    modifier: Modifier = Modifier,
+    tint: Color = Color.Unspecified
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_equipment),
+        contentDescription = "equipment",
+        modifier = modifier,
+        tint = tint
+    )
+}
+
+@Composable
+fun Filter(
+    modifier: Modifier = Modifier,
+    tint: Color = Color.Unspecified
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_filter),
+        contentDescription = "filter",
+        modifier = modifier,
+        tint = tint
+    )
+}
+
+@Composable
+fun GYMILogo(
+    modifier: Modifier = Modifier,
+    tint: Color = Color.Unspecified
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_gymi_logo),
+        contentDescription = "gymiLogo",
+        modifier = modifier,
+        tint = tint
+    )
+}
+
+@Composable
+fun Home(
+    modifier: Modifier = Modifier,
+    tint: Color = Color.Unspecified
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_home),
+        contentDescription = "home",
+        modifier = modifier,
+        tint = tint
+    )
+}
+
+@Composable
+fun Modifier(
+    modifier: Modifier = Modifier,
+    tint: Color = Color.Unspecified
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_modifier),
+        contentDescription = "modifier",
+        modifier = modifier,
+        tint = tint
+    )
+}
+
+@Composable
+fun Notice(
+    modifier: Modifier = Modifier,
+    tint: Color = Color.Unspecified
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_notice),
+        contentDescription = "notice",
+        modifier = modifier,
+        tint = tint
+    )
+}
+
+@Composable
+fun Plus(
+    modifier: Modifier = Modifier,
+    tint: Color = Color.Unspecified
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_plus),
+        contentDescription = "plus",
+        modifier = modifier,
+        tint = tint
+    )
+}
+
+@Composable
+fun Profile(
+    modifier: Modifier = Modifier,
+    tint: Color = Color.Unspecified
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_profile),
+        contentDescription = "profile",
+        modifier = modifier,
+        tint = tint
+    )
+}
+
+@Composable
+fun Reservation(
+    modifier: Modifier = Modifier,
+    tint: Color = Color.Unspecified
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_reservation),
+        contentDescription = "reservation",
+        modifier = modifier,
+        tint = tint
+    )
+}
+
+@Composable
+fun Volleyball(
+    modifier: Modifier = Modifier,
+    tint: Color = Color.Unspecified
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_volleyball),
+        contentDescription = "volleyball",
+        modifier = modifier,
+        tint = tint
+    )
+}
+
+@Composable
+fun X(
+    modifier: Modifier = Modifier,
+    tint: Color = Color.Unspecified
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_x),
+        contentDescription = "x",
+        modifier = modifier,
+        tint = tint
+    )
+}
