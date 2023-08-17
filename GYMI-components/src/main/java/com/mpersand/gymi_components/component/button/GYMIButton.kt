c70733992c14c6ef4979cb940af63f6a73e37938
박성현 <pjhomg5530@naver.com>
2023-08-17 13:44:47 +0900

    :lipstick: ::  contentPadding 추가

--- GYMI-components/src/main/java/com/mpersand/gymi_components/component/button/GYMIButton.kt
+++ GYMI-components/src/main/java/com/mpersand/gymi_components/component/button/GYMIButton.kt
@@ -1,5 +1,6 @@
 package com.mpersand.gymi_components.component.button
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
@@ -8,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.mpersand.gymi_components.theme.GYMITheme
 import com.mpersand.gymi_components.theme.White
@@ -18,6 +20,7 @@ fun GYMIButton(
     text: String,
     backgroundColor: Color = GYMITheme.colors.p1,
     style: TextStyle = GYMITheme.typography.subtitle3,
+    contentPadding: Dp = 15.dp,
     onClick: () -> Unit
 ) {
     Button(
@@ -27,7 +30,8 @@ fun GYMIButton(
             backgroundColor = backgroundColor,
             contentColor = White
         ),
-        shape = RoundedCornerShape(8.dp)
+        shape = RoundedCornerShape(8.dp),
+        contentPadding = PaddingValues(vertical = contentPadding)
     ) {
         Text(
             text = text,
