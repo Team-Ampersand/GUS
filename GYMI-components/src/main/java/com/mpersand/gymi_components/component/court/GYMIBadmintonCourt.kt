51ad44dcefce48da42ee2cf2b76bc87bda9cc3bf
hyeonseo <s21035@gsm.hs.kr>
2023-08-22 09:59:34 +0900

    :lipstick: :: 반 코트로 변경

--- GYMI-components/src/main/java/com/mpersand/gymi_components/component/court/GYMIBadmintonCourt.kt
+++ GYMI-components/src/main/java/com/mpersand/gymi_components/component/court/GYMIBadmintonCourt.kt
@@ -2,7 +2,6 @@ package com.mpersand.gymi_components.component.court
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -11,7 +10,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import com.mpersand.gymi_components.R
@@ -21,34 +19,27 @@ import com.mpersand.gymi_components.theme.White
 @Composable
 fun GYMIBadmintonCourt(
     modifier: Modifier = Modifier,
-    isReserved: Int,
+    isReserved: Boolean,
     onClick: () -> Unit
 ) {
-    var isClicked by remember { mutableStateOf(-1) }
+    var isClicked by remember { mutableStateOf(false) }
 
-    Column(modifier = modifier) {
-        repeat(2) {
-            Image(
-                modifier = Modifier
-                    .clickable {
-                        isClicked = if (isClicked == it) -1 else it
-                        onClick()
-                    }
-                    .graphicsLayer {
-                        if (it % 2 != 0) rotationX = 180f
-                    },
-                painter = painterResource(id = R.drawable.ic_badminton_court),
-                contentDescription = "badminton_court",
-                contentScale = ContentScale.FillBounds,
-                colorFilter = ColorFilter.tint(
-                    color = when {
-                        isClicked == it && isReserved != it -> GYMITheme.colors.positive
-                        isReserved == it -> GYMITheme.colors.error
-                        else -> White
-                    },
-                    blendMode = BlendMode.Darken
-                )
-            )
-        }
-    }
+    Image(
+        modifier = modifier
+            .clickable {
+                isClicked = !isClicked
+                onClick()
+            },
+        painter = painterResource(id = R.drawable.ic_badminton_court),
+        contentDescription = "badminton_court",
+        contentScale = ContentScale.FillBounds,
+        colorFilter = ColorFilter.tint(
+            color = when {
+                isClicked && !isReserved -> GYMITheme.colors.positive
+                isReserved -> GYMITheme.colors.error
+                else -> White
+            },
+            blendMode = BlendMode.Darken
+        )
+    )
 }
