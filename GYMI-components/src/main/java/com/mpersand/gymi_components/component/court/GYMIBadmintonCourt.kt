09056cf4aceda60535e89f160ef130015c6aa1df
hyeonseo <s21035@gsm.hs.kr>
2023-09-25 20:24:59 +0900

    :sparkles: :: onLongclick 추가

--- GYMI-components/src/main/java/com/mpersand/gymi_components/component/court/GYMIBadmintonCourt.kt
+++ GYMI-components/src/main/java/com/mpersand/gymi_components/component/court/GYMIBadmintonCourt.kt
@@ -1,7 +1,9 @@
 package com.mpersand.gymi_components.component.court
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -16,16 +18,19 @@ import com.mpersand.gymi_components.R
 import com.mpersand.gymi_components.theme.GYMITheme
 import com.mpersand.gymi_components.theme.White
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun GYMIBadmintonCourt(
     modifier: Modifier = Modifier,
     isReserved: Boolean,
-    onClick: () -> Unit
+    onClick: () -> Unit,
+    onLongClick: () -> Unit
 ) {
     Image(
-        modifier = modifier.clickable {
-            onClick()
-        },
+        modifier = modifier.combinedClickable(
+            onClick = onClick,
+            onLongClick = onLongClick
+        ),
         painter = painterResource(id = R.drawable.ic_badminton_court),
         contentDescription = "badminton_court",
         contentScale = ContentScale.FillBounds,
